60bc3e0e8de2d5b748a6ae78b10df17a572ad62d
LokemTotem <sw19912@gmail.com>
2017-01-13 20:07:40 -0500

    elephant feet
    implmented stomps

---
+++ Assets/Scripts/elephant_Ctrl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elephant_Ctrl : MonoBehaviour {
+
+    //random stomp time
+    //slct nxt stomp
+
+    public float stompTimer = 1f;
+    public float nxtStomp;
+    public float minStompTimr;
+    public float maxStompTimr;
+
+    public GameObject[] feetArray;
+
+    public int footSelect = 0;
+
+
+    void Update ()
+    {
+        nxtStomp -= Time.deltaTime;
+
+        if (nxtStomp < 0)
+            StompTimr();
+    }
+
+
+    void StompTimr()
+    {
+        //foot selct
+        footSelect++;
+
+        if (footSelect > 3)
+            footSelect = 0;
+
+        //foot animation and follow
+        feetArray[footSelect].GetComponent<footMove>().enabled = true;
+        feetArray[footSelect].transform.position = Vector3.Lerp(feetArray[footSelect].transform.position, feetArray[footSelect].transform.position + new Vector3(0,8,0), stompTimer);
+
+        //delay
+        nxtStomp = Random.Range(minStompTimr, maxStompTimr);
+
+    }
+
+
+}
