5d8c7d64daccb1629b329cca66bc6860d69fee3d
Matthew Ferreira <ChronoblastGamesOfficial@gmail.com>
2017-01-14 17:38:40 -0500

    Started work on MapGeneration

--- Assets/Scripts/Systems/Map/MapController.cs
+++ Assets/Scripts/Systems/Map/MapController.cs
@@ -5,32 +5,48 @@ using UnityEngine;
 public class MapController : MonoBehaviour
 {
     [Header("Generation Attributes")]
+    private GameObject roomHolder;
+
     public GameObject[] roomPrefabs;
+    public GameObject exitPrefab;
+
     public GameObject currentRoom;
+    public GameObject entranceAnchor;
 
-    public List<GameObject> exitAnchorList;
+    void Start()
+    {
+        roomHolder = GameObject.FindGameObjectWithTag("RoomHolder");
 
-    public void GenerateNextRoom()
+        GenerateFirstRoom();
+    }
+
+    public void GenerateFirstRoom()
+    {
+        entranceAnchor = GameObject.Find("EntranceAnchor");
+        GameObject newRoom = Instantiate(roomPrefabs[0], entranceAnchor.transform.position, entranceAnchor.transform.rotation, roomHolder.transform);
+        currentRoom = newRoom;
+        GenerateRoomExit(newRoom);
+    }
+
+    public void GenerateNextRoom(GameObject newRoom)
+    {
+       
+    }
+
+    public void GenerateRoomExit(GameObject newRoom)
+    {
+        Transform exitHolder = newRoom.transform.GetChild(0).FindChild("ExitAnchors");
+        Transform[] exitArray = exitHolder.GetComponentsInChildren<Transform>();
+        int randValue = Random.Range(0, exitArray.Length);
+        GameObject newExit = Instantiate(exitPrefab, exitArray[randValue].transform.position, exitArray[randValue].transform.rotation);
+    }
+
+    public void GenerateObstacles(GameObject newRoom)
     {
 
     }
 
-    public void GenerateExit()
-    {
-
-    }
-
-    public void GenerateEntrance()
-    {
-
-    }
-
-    public void GenerateObstacles()
-    {
-
-    }
-
-    public void GenerateCheese()
+    public void GenerateCheese(GameObject newRoom)
     {
 
     }
